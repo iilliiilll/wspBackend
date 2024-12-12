f83536034221bb27d7ee9d4fa7c43e31303c2d0d
soowan <tndhks0708@naver.com>
2024-12-12 13:32:24 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20200839/wspBackend/WspBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20200839.wspBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WspBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<WspBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20200839/wspBackend/WspBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20200839.wspBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WspBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
